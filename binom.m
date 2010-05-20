e5dc3fc0740ba6d5837d4e41dfec649c69e56587
Alan Lue <alan.lue@gmail.com>
2010-05-20 04:16:58 -0700

    Added fixed dividend yield option to binomial method.

--- binom.m
+++ binom.m
@@ -1,6 +1,13 @@
-function [opt] = binom(S, X, r, T, sd, q, fCallPut, fAmEur, n)
+function [opt] = binom(S, X, r, T, sd, fCallPut, fAmEur, n, q, D)
 %BINOM Binomial put and call pricing.
 %   [OPT] = BINOM() prices an option using a binomial pricing model.
+%
+%   Assumes dividends are paid quarterly and that the last dividend
+%   is paid one-half month prior to expiration.  The dividend yield
+%   q is specified as an annual percentage.
+
+if nargin < 10, D = 0; end
+if nargin < 9, q = 0; end
 
 dt = T/n;
 u = exp(sd*sqrt(dt));
@@ -8,11 +15,16 @@ d = 1/u;
 p = (exp(r*dt) - d) / (u - d);
 
 S_T = S * u.^(n:-1:0) .* d.^(0:n);
+if q > 0                                % Account for dividend yield.
+    nq = floor(4*(T - 0.5/12) + 1);
+    S_T = S_T * (1 - q/4)^nq;
+end
 if (fCallPut)                           % Option is call.
     v = bsxfun(@max, S_T - repmat(X, [1 n+1]), 0);
 else                                    % Option is put.
     v = bsxfun(@max, repmat(X, [1 n+1]) - S_T, 0);
 end
+
 for i = n-1:-1:0
     v_u = v(1:end-1);
     v_d = v(2:end);
@@ -20,6 +32,10 @@ for i = n-1:-1:0
     
     if (fAmEur)                         % Option is American.
         S_i = S * u.^(i:-1:0) .* d.^(0:i);
+        if q > 0                        % Account for dividend yield.
+            nq = floor(4*(i*dt - 0.5/12) + 1);
+            S_i = S_i * (1 - q/4)^nq;
+        end
         if (fCallPut)                   % Option is a call.
             ev = bsxfun(@max, S_i - repmat(X, [1 i+1]), 0);
         else                            % Option is a put.
@@ -30,4 +46,5 @@ for i = n-1:-1:0
         v = cv;
     end
 end
+
 opt = v;
--- d_ps_1.m
+++ d_ps_1.m
@@ -8,10 +8,23 @@ q = 0;
 fCallPut = 0;                           % fCallPut == 0 => put
 fAmEur = 1;                             % fAmEur == 1 => american
 
-fdi1(S, X, r, T, sd, q, fCallPut, fAmEur)
-fdi2(S, X, r, T, sd, q, fCallPut, fAmEur)
-fde1(S, X, r, T, sd, q, fCallPut, fAmEur)
-fde2(S, X, r, T, sd, q, fCallPut, fAmEur)
+fdi1(S, X, r, T, sd, fCallPut, fAmEur, q)
+fdi2(S, X, r, T, sd, fCallPut, fAmEur, q)
+fde1(S, X, r, T, sd, fCallPut, fAmEur, q)
+fde2(S, X, r, T, sd, fCallPut, fAmEur, q)
+
+%% Scratchwork.
+S = 40;
+X = 35;
+r = 0.05;                               % Annual.
+T = 1 / 12;                             % Months / 12.
+sd = 0.30;                              % Annual.
+q = 0.05;                               % Annual.
+fCallPut = 1;                           % fCallPut == 1 => call
+fAmEur = 1;                             % fAmEur == 1 => american
+
+n = struct('bin1', 140);
+binom(S, X, r, T, sd, fCallPut, fAmEur, n.bin1, q)
 
 %% Table 1.
 S = 40;
@@ -26,11 +39,11 @@ fAmEur = 1;                             % fAmEur == 1 => american
 i = 1; j = 1;
 n = struct('bin', 300, 'fde1', 315*12*T(j), 'fd', 45*12*T(j));
 m = struct('fde1', 200, 'fd', 200);
-binom(S, X(i), r, T(j), sd, q, fCallPut, fAmEur, n.bin)
-fde1(S, X(i), r, T(j), sd, q, fCallPut, fAmEur, n.fde1, m.fde1)
-fde2(S, X(i), r, T(j), sd, q, fCallPut, fAmEur, n.fd, m.fd)
-fdi1(S, X(i), r, T(j), sd, q, fCallPut, fAmEur, n.fd, m.fd)
-fdi2(S, X(i), r, T(j), sd, q, fCallPut, fAmEur, n.fd, m.fd)
+binom(S, X(i), r, T(j), sd, fCallPut, fAmEur, n.bin, q)
+fde1(S, X(i), r, T(j), sd, fCallPut, fAmEur, n.fde1, m.fde1, q)
+fde2(S, X(i), r, T(j), sd, fCallPut, fAmEur, n.fd, m.fd, q)
+fdi1(S, X(i), r, T(j), sd, fCallPut, fAmEur, n.fd, m.fd, q)
+fdi2(S, X(i), r, T(j), sd, fCallPut, fAmEur, n.fd, m.fd, q)
 
 
 %% Following code not vetted.
